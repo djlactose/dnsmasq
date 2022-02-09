54d078d8bc4a3027c52fdda525d6ebcf7361e51b
Nick Hernandez <nick.hernandez@zydoc.com>
2022-02-09 08:35:36 -0500

    Updated to add another redudant dns providers as sources

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
-echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+echo "nameserver 1.1.1.1" >> /etc/resolv.conf
+echo "nameserver 208.67.222.222" >> /etc/resolv.conf
 dnsmasq --bind-interfaces --cache-size=1500 --dns-forward-max=2048 --no-daemon --addn-hosts=/root/data/hosts
