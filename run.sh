e7538f1662ff89b673ba79f72abaf34d8dfbe59c
Nick Hernandez <nickhernandez@djlactose.com>
2022-11-30 21:17:51 -0500

    Changed to use keep in forground instead of no daemon mode

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ echo "nameserver 1.1.1.1" >> /etc/resolv.conf
 echo "nameserver 208.67.222.222" >> /etc/resolv.conf
 echo "Added Name Servers"
 if [[ "$Debug" == "false" ]]; then
-    dnsmasq --bind-interfaces --cache-size=1500 -D -b -f --dns-forward-max=2048 --no-daemon --log-queries --log-facility=LOCAL0 --addn-hosts=/root/data/hosts
+    dnsmasq --bind-interfaces --cache-size=1500 -D -b -f --dns-forward-max=2048 --keep-in-foreground --log-queries --log-facility=LOCAL0 --addn-hosts=/root/data/hosts
 else
     echo "Starting with debug on"
-    dnsmasq --bind-interfaces --cache-size=1500 -D -b -f --dns-forward-max=2048 --no-daemon --log-debug --log-queries --log-facility=LOCAL0 --addn-hosts=/root/data/hosts
+    dnsmasq --bind-interfaces --cache-size=1500 -D -b -f --dns-forward-max=2048 --keep-in-foreground --log-debug --log-queries --log-facility=LOCAL0 --addn-hosts=/root/data/hosts
 fi
