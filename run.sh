887cdcc7c15d8b06baf4cc0f249f947aff93737a
Nick Hernandez <nick.hernandez@zydoc.com>
2022-12-02 16:36:10 -0500

    Removed hardcoded dns settings.

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
-echo "nameserver 1.1.1.1" >> /etc/resolv.conf
-echo "nameserver 208.67.222.222" >> /etc/resolv.conf
 echo "Added Name Servers"
 if [[ "$Debug" == "false" ]]; then
     dnsmasq --bind-interfaces --cache-size=1500 -D -b -f --dns-forward-max=2048 --keep-in-foreground --log-queries --log-facility=LOCAL0 --addn-hosts=/root/data/hosts
